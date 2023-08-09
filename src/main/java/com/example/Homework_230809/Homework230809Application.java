6ef8eab521e763053d97beb766e26dd690c368f1
Tavkel <gmininb@gmail.com>
2023-08-09 17:47:16 +0300

    Initial commit

---
+++ src/main/java/com/example/Homework_230809/Homework230809Application.java
@@ -0,0 +1,13 @@
+package com.example.Homework_230809;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework230809Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework230809Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Homework_230809/Homework230809ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Homework_230809;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework230809ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
